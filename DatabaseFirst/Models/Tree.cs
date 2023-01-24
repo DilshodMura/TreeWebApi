fe308469d8e40060d4e1db0d90bdfb4727793683
Dilshod Muratov <muratovdilshod2@gmail.com>
2023-01-24 10:30:27 +0500

    DbFirst has been added

---
+++ DatabaseFirst/Models/Tree.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DatabaseFirst.Models;
+
+public partial class Tree
+{
+    public int Id { get; set; }
+
+    public int Sort { get; set; }
+
+    public int Type { get; set; }
+
+    public double MaxHeight { get; set; }
+
+    public double MaxSquare { get; set; }
+
+    public double MaxFruitlness { get; set; }
+
+    public int Amount { get; set; }
+
+    public int AreaId { get; set; }
+
+    public virtual FarmArea Area { get; set; } = null!;
+}
---
+++ DatabaseFirst/Models/TreeDbFirstContext.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatabaseFirst.Models;
+
+public partial class TreeDbFirstContext : DbContext
+{
+    public TreeDbFirstContext()
+    {
+    }
+
+    public TreeDbFirstContext(DbContextOptions<TreeDbFirstContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<FarmArea> FarmAreas { get; set; }
+
+    public virtual DbSet<Tree> Trees { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=nba-091-01-UZ\\SQLEXPRESS;Database=TreeDbFirst; Integrated Security=SSPI;Trusted_Connection=True;TrustServerCertificate=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<FarmArea>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__FarmArea__3214EC07935FCD6C");
+
+            entity.ToTable("FarmArea");
+
+            entity.Property(e => e.Id).ValueGeneratedNever();
+        });
+
+        modelBuilder.Entity<Tree>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Trees__3214EC07239C488D");
+
+            entity.Property(e => e.Id).ValueGeneratedNever();
+
+            entity.HasOne(d => d.Area).WithMany(p => p.Trees)
+                .HasForeignKey(d => d.AreaId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__Trees__AreaId__398D8EEE");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
